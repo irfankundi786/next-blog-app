c2f7a8a4ded9d5c73254ffa0a892c33efc3f7265
khan786 <131508962+irfankundi786@users.noreply.github.com>
2024-08-21 21:36:42 +0500

    Fix next.config.js structure and configure webpack alias

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
   images: {
     domains: ["www.pexels.com", "images.pexels.com"],
   },
